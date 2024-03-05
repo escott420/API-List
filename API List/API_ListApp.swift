432d130862cc5ce451e10b9d6331b147e0cf25ac
Ethan <ethan.scott2@icloud.com>
2024-03-05 09:40:01 -0600

    Initial Commit

---
+++ API List/API_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_ListApp.swift
+//  API List
+//
+//  Created by Ethan Scott on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct API_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
